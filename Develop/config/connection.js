cc89b738362c83b57a4bff2dc1373f78b326e70d
Dylan Grissom <dgriss295@gmail.com>
2023-05-08 18:39:15 -0500

    removed pw

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', 'DGcity23!', {
+  : new Sequelize('ecommerce_db', 'your_username', 'your_password', {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
